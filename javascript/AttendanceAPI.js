4991139b1d6e3f261b3de23d5ecdee9f36da2db3
amin bislimaj <amin.b05@icloud.com>
2026-04-17 13:17:25 +0200

    v0.2.5

--- javascript/AttendanceAPI.js
+++ javascript/AttendanceAPI.js
@@ -17,7 +17,7 @@ export async function validateCode(data) {
         Authorization: `Bearer ${token}`,
         "Content-Type": "application/json",
       },
-      body: JSON.stringify(data), // ✅ FIXED
+      body: JSON.stringify(data), 
     },
   );
 
